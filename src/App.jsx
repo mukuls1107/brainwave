764e8ef72c47eafa0708a7de84ed081bde63c1fe
mukuls1107 <mukulsingh3078@gmail.com>
2025-04-28 22:17:21 +0530

    updated the navigation menu

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,6 @@ import Header from "./components/Header";
 function App() {
   return (
     <>
-      {/* <h1 className="text-3xl font-bold underline">Hello brainwave!</h1> */}
-
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
       </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import { brainwave } from "../assets";
 import { useLocation } from "react-router-dom";
 import { navigation } from "../constants/index";
 import Button from "./Button";
+import MenuSvg from "../assets/svg/MenuSvg";
+import { HambugerMenu } from "./design/Header";
+
 const Header = () => {
   const pathName = useLocation();
+  const [openNavigation, setOpenNavigation] = useState(false);
+
+  const toggleNavigation = () => {
+    if (openNavigation) {
+      setOpenNavigation(false);
+    } else {
+      setOpenNavigation(true);
+    }
+  };
+
+  const handleClick = () => {
+    setOpenNavigation(false);
+  };
   return (
-    <div className="fixed top-0 left-0 w-full z-50 bg-n-8/90 backdrop-blur-sm border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm">
+    <div
+      className={`fixed top-0 left-0 w-full z-50 bg-n-8/90 backdrop-blur-sm border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm ${
+        openNavigation ? "bg-n-8" : "bg-n-8/90 backdrop-blur-sm"
+      }`}
+    >
       <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
         <a href="#hero" className="block w-[12rem] xl:mr-8">
           <img src={brainwave} alt="" width={190} height={40} />
         </a>
 
-        <nav className="hidden fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent">
+        <nav
+          className={`${
+            openNavigation ? "flex" : "hidden"
+          } fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent `}
+        >
           <div className="relative z-2 flex flex-col items-center m-auto justify-center lg:flex-row">
             {navigation.map((item) => (
               <a
@@ -29,6 +53,7 @@ const Header = () => {
                 {item.title}
               </a>
             ))}
+            <HambugerMenu />
           </div>
         </nav>
 
@@ -42,6 +67,13 @@ const Header = () => {
         <Button className="hidden lg:flex" href="#login">
           {" "}
           Sign In
+        </Button>
+        <Button
+          className="ml-auto lg:hidden"
+          px="px-3"
+          onClick={toggleNavigation}
+        >
+          <MenuSvg openNavigation={openNavigation}></MenuSvg>
         </Button>
       </div>
     </div>
